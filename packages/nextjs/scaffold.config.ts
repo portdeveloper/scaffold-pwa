22f0ce8aa23100c2bea62978b2989e404eebd5de
port <108868128+portdeveloper@users.noreply.github.com>
2025-03-29 12:56:10 +0300

    Point app to sepolia

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -13,7 +13,7 @@ export const DEFAULT_ALCHEMY_API_KEY = "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
 
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [chains.foundry],
+  targetNetworks: [chains.sepolia],
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect if you only target the local network (default is 4000)
