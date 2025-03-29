23f6ec926904605e76203eb02a1691ea0e46a4ec
port <108868128+portdeveloper@users.noreply.github.com>
2025-03-29 12:58:51 +0300

    Modify next.config.ts

--- packages/nextjs/next.config.ts
+++ packages/nextjs/next.config.ts
@@ -1,19 +1,38 @@
+import { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } from "next/constants.js";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  devIndicators: false,
-  typescript: {
-    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
-  },
-  eslint: {
-    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
-  },
-  webpack: config => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
-  },
+let nextConfig: NextConfig = {};
+
+/** @type {(phase: string, defaultConfig: import("next").NextConfig) => Promise<import("next").NextConfig>} */
+export default async (phase: any) => {
+  /** @type {import("next").NextConfig} */
+  nextConfig = {
+    reactStrictMode: true,
+    devIndicators: false,
+    typescript: {
+      ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    },
+    eslint: {
+      ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    },
+    webpack: config => {
+      config.resolve.fallback = { fs: false, net: false, tls: false };
+      config.externals.push("pino-pretty", "lokijs", "encoding");
+      return config;
+    },
+  };
+
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withSerwist = (await import("@serwist/next")).default({
+      // Note: This is only an example. If you use Pages Router,
+      // use something else that works, such as "service-worker/index.ts".
+      swSrc: "app/sw.ts",
+      swDest: "public/sw.js",
+    });
+    return withSerwist(nextConfig);
+  }
+
+  return nextConfig;
 };
 
 const isIpfs = process.env.NEXT_PUBLIC_IPFS_BUILD === "true";
